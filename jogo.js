7d0cc58c37416571829748091c80a591034654c2
MaxwelSantana <maxwelsant96@icloud.com>
2020-07-29 07:12:50 -0300

    added initial screen and separate them into object

--- jogo.js
+++ jogo.js
@@ -88,14 +88,57 @@ const flappyBird = {
     }
 }
 
-function loop() {
-    flappyBird.atualiza();
-    planoDeFundo.desenha();
-    chao.desenha();
+const mensagemGetReady = {
+    sX: 134,
+    sY: 0,
+    w: 174,
+    h: 152,
+    x: (canvas.width / 2) - 174 / 2,
+    y: 50,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            mensagemGetReady.sX, mensagemGetReady.sY,
+            mensagemGetReady.w, mensagemGetReady.h,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.w, mensagemGetReady.h
+        );
+    }
+}
 
-    flappyBird.desenha();
+let telaAtiva = {};
+function mudaParaTela(novaTela) {
+    telaAtiva = novaTela;
+}
+
+const Telas = {
+    INICIO: {
+        atualiza() {},
+        desenha() {
+            planoDeFundo.desenha();
+            chao.desenha();
+            flappyBird.desenha();
+            mensagemGetReady.desenha();
+        }
+    },
+    JOGO: {
+        atualiza() {
+            flappyBird.atualiza();
+        },
+        desenha() {
+            planoDeFundo.desenha();
+            chao.desenha();
+            flappyBird.desenha();
+        }
+    }
+};
+
+function loop() {
+    telaAtiva.atualiza();
+    telaAtiva.desenha();
 
     requestAnimationFrame(loop);
 }
 
+mudaParaTela(Telas.INICIO);
 loop();
